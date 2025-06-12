31ed57f4fa312941e98ce4d1b8db12ab86e59914
admachacon <admachacon@gmail.com>
2025-06-12 06:21:10 -0400

    update general routes 5 and 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,25 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+    // Check if the user does not already exist
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    if (userswithsamename.length == 0) {
+      // Add the new user to the users array
+      users.push({"username": username, "password": password});
+      return res.status(200).json({message: "User successfully registered."});
+    } else {
+      return res.status(404).json({message: "User already exists."});
+    }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
@@ -47,16 +64,7 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  let review = {};
-//   book = JSON.parse(books[isbn]);
-  const obj = JSON.parse(text, function (key, value) {
-  Object.keys(books[isbn]).forEach(key => {
-  if (key == "review") {
-    review = value;
-  } else {
-    return value;
-  }
-  res.send(JSON.stringify(books[isbn],null,4));
+  res.send(JSON.stringify(books[isbn].reviews,null,4));
 });
 
 module.exports.general = public_users;
