69d0f3e825eef1ec301d00d099e992d9d24fdb65
admachacon <admachacon@gmail.com>
2025-06-12 04:31:32 -0400

    update general routes 1 and 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,19 +12,25 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
+  const author = req.params.author;
+// let filtered_books = books.filter((book) => book.author === author);
+// let filtered_books = JSON.stringify(books)
+// filtered_books = find_in_object(JSON.parse(filtered_books), {author: author});
+// filtered_books = Object.entries(books).filter(([row])=> !filterDialogView.includes(row))
+//   console.log(books.length());
+//   const filtered_books = books.filter((book) => book.author === author);
+//   res.send(filtered_books);
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
